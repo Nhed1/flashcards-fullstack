172231e8bd55f7fca2aa2554e3337d0c56648de8
leonardo <leonardogarreto@gmail.com>
2023-07-09 17:10:39 -0300

    feat: add name prop to deck model

--- app/api/decks/route.ts
+++ app/api/decks/route.ts
@@ -2,6 +2,11 @@ import { prisma } from "../db";
 import { getAuth } from "@clerk/nextjs/server";
 import type { NextApiRequest, NextApiResponse } from "next";
 
+type NextApiRequestWithFormData = NextApiRequest &
+  Request & {
+    files: any[];
+  };
+
 export async function GET(req: NextApiRequest, res: NextApiResponse) {
   const decks = await prisma.deck.findMany();
 
@@ -10,13 +15,18 @@ export async function GET(req: NextApiRequest, res: NextApiResponse) {
   return res.status(200).json({ decks });
 }
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
+export async function POST(
+  req: NextApiRequestWithFormData,
+  res: NextApiResponse
+) {
   const { userId } = getAuth(req);
+  const { name }: { name: string } = await req.json();
 
   if (!userId) return res.status(401).json({ error: "unauthorized" });
 
   const deck = await prisma.deck.create({
     data: {
+      name,
       userId,
     },
   });
--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,31 @@
 "use client";
 
 import { UserButton } from "@clerk/nextjs";
+import { useState } from "react";
 
 export default function Home() {
+  const [deckName, setDeckName] = useState("");
+
   const handleNewDeck = async () => {
     await fetch("/api/decks", {
       method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: deckName,
+      }),
     });
   };
 
   return (
     <>
       <UserButton showName />
+      <input
+        className="border-black"
+        type="text"
+        onChange={(e) => setDeckName(e.target.value)}
+      />
       <button onClick={handleNewDeck}>Create new deck</button>
     </>
   );
