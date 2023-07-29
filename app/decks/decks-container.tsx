3a517699baae8ae62342e67bef4efc4177217549
leonardo <leonardogarreto@gmail.com>
2023-07-29 19:59:58 -0300

    fix: remove test button

--- app/decks/decks-container.tsx
+++ app/decks/decks-container.tsx
@@ -6,23 +6,15 @@ import { CreateDeckModal } from "./create-deck/create-deck-modal";
 import DecksTable from "./decks-table";
 import { ErrorBoundary } from "react-error-boundary";
 import Error from "./error";
-import { useEffect } from "react";
 
 export default function DecksContainer() {
   const { isOpen, onOpen, onClose } = useDisclosure();
-
-  const getFlashcards = async () => {
-    const res = await fetch("/api/flashcards/1");
-
-    console.log(res);
-  };
 
   return (
     <VStack mt="10" gap="8">
       <Button variant="solid" onClick={onOpen}>
         add new deck
       </Button>
-      <Button onClick={getFlashcards}>Teste</Button>
       <ErrorBoundary fallback={<Error />}>
         <DecksTable />
       </ErrorBoundary>
