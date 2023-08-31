a1747a8ea488a6a7e89cc802eb0cd8c995d538b4
leonardo <leonardogarreto@gmail.com>
2023-08-31 12:23:37 -0300

    fix: header name

--- app/home-header.tsx
+++ app/home-header.tsx
@@ -1,4 +1,4 @@
-import { Flex, Heading } from "@chakra-ui/react";
+import { Box, Flex, Heading, Text } from "@chakra-ui/react";
 import { UserButton } from "@clerk/nextjs";
 
 export default function HomeHeader() {
@@ -7,14 +7,17 @@ export default function HomeHeader() {
       position="sticky"
       top="0"
       justify="space-between"
+      alignItems="center"
       p="3"
       pr="10"
       backgroundColor="blackAlpha.800"
       textColor="gray.100"
       w="100%"
     >
-      <Heading size="lg">Flashcards APP</Heading>
-
+      <Box>
+        <Heading size="lg">Learnly Cards</Heading>
+        <Text fontSize="small">flashcards application to learn English</Text>
+      </Box>
       <UserButton />
     </Flex>
   );
