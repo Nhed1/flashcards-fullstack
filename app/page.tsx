f5aea197b279836811f6b336741d3619f4636ff0
leonardo <leonardogarreto@gmail.com>
2023-09-07 15:51:40 -0300

    fix: minor signed in issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 "use client";
 
 import { Flex } from "@chakra-ui/react";
-import { SignIn } from "@clerk/nextjs";
+import { SignIn, useAuth } from "@clerk/nextjs";
 import HomeHeader from "./home-header";
+import { useEffect } from "react";
+import { redirect } from "next/navigation";
 
 export default function Home() {
+  const { isSignedIn } = useAuth();
+
+  useEffect(() => {
+    if (isSignedIn) {
+      redirect("/home");
+    }
+  }, [isSignedIn]);
+
   return (
     <Flex
       justify="center"
